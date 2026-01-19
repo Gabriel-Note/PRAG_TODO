628bc9ecf1f9cd5b989987ac94e28e96c51b98f4
gabriel note <gabriel.emil.note@gmail.com>
2026-01-19 15:15:04 +0100

    löser den skapade konflikten

--- src/main/java/JavaGrundFortsattning/PRAG_TODO/controller/ProductController.java
+++ src/main/java/JavaGrundFortsattning/PRAG_TODO/controller/ProductController.java
@@ -5,5 +5,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ProductController {
     // skjhgkdlmkbnsjlrd
+    // nu sitter jag i min branch
+// DSIFJOGNSLBMSÄBKE
+//
+//
+// ASCSCSDV
+    // WANODF
+    // testar detta lite snabbt nu!
+
     // gkmdofmdmkkms
 }
