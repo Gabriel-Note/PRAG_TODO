8a28188c3c7a44c2e6cbea0ac12623ffbb017434
LialviSWE <alexbranderlund@gmail.com>
2026-01-19 14:59:21 +0100

    Testar felsökning

--- src/main/java/JavaGrundFortsattning/PRAG_TODO/controller/ProductController.java
+++ src/main/java/JavaGrundFortsattning/PRAG_TODO/controller/ProductController.java
@@ -4,11 +4,5 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
-    // skjhgkdlmkbnsjlrd
-    // nu sitter jag i min branch
-// DSIFJOGNSLBMSÄBKE
-//
-//
-// ASCSCSDV
-    // WANODF
+    // testar detta lite snabbt nu!
 }
