ba3b068dcba06321f68a6d62a11fcef99ec967d7
gabriel note <gabriel.emil.note@gmail.com>
2026-01-26 14:44:33 +0100

    La till variabler, konstruktor, getters och setters

--- src/main/java/JavaGrundFortsattning/PRAG_TODO/dto/CreateTaskDto.java
+++ src/main/java/JavaGrundFortsattning/PRAG_TODO/dto/CreateTaskDto.java
@@ -1,4 +1,20 @@
 package JavaGrundFortsattning.PRAG_TODO.dto;
 
 public class CreateTaskDto {
+    private String name;
+
+    public CreateTaskDto() {
+    }
+
+    public CreateTaskDto(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
