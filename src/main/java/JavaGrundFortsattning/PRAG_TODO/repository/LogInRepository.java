b646ec886957625fef80b70d4b6180a06eca1292
pogo_56 <pouya1980@gmail.com>
2026-02-25 19:39:00 +0100

    entity klar Dto klar repository klar

--- src/main/java/JavaGrundFortsattning/PRAG_TODO/repository/LogInRepository.java
+++ src/main/java/JavaGrundFortsattning/PRAG_TODO/repository/LogInRepository.java
@@ -1,4 +1,8 @@
 package JavaGrundFortsattning.PRAG_TODO.repository;
 
-public interface LogInRepository {
+import JavaGrundFortsattning.PRAG_TODO.entity.LogIn;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LogInRepository extends JpaRepository<LogIn, Long> {
+    LogIn findByUsername(String username);
 }
